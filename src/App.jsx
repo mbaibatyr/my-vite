f103d745d08016bbd6319e9dfe87f0cef3ff8bf0
mbaibatyr <murat_b@mail.ru>
2025-03-17 21:17:31 +0500

    ++

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import { Button } from "antd";
+import { Button, Select } from "antd";
 import "./App.css";
+//import { MyComp, Multiply } from "./Functions";
+import * as f from "./Functions";
 
 const App = () => {
-  const [val, setVal] = useState(0);
-  const [val2, setVal2] = useState(0);
-  const [arr, setArr] = useState(["1", "2", "3"]);
+  // const [val, setVal] = useState(0);
+  // const [val2, setVal2] = useState(0);
+  // const [arr, setArr] = useState(["1", "2", "3"]);
 
   // useEffect(() => {
   //   console.log("Компонент создался");
@@ -23,10 +25,31 @@ const App = () => {
   //   console.log("Компонент изменился2");
   // });
 
+  let a = "50";
+  const res = f.Multiply(a, 8);
   return (
     <>
-      App
-      <button onClick={() => setVal(val + 1)}>Кнопка1</button>
+      <Select
+        style={{
+          width: 500,
+        }}
+        placeholder="Choose City"
+        showSearch
+        status="success"
+        optionFilterProp="children"
+        //onChange={handleChangeProcess}
+        filterOption={(input, option) =>
+          option.children.toLowerCase().includes(input.toLowerCase())
+        }
+      >
+        {f.Files.map((z) => (
+          <Option key={z.id}>{z.name}</Option>
+        ))}
+      </Select>
+
+      {/* {res}
+      <f.MyComp a={5} b={6} /> */}
+      {/* <button onClick={() => setVal(val + 1)}>Кнопка1</button>
       <button onClick={() => setVal2(val2 + 1)}>Кнопка2</button>
       <ul>
         {arr.map((z, index) => (
@@ -35,7 +58,7 @@ const App = () => {
       </ul>
       <Button type="dashed" onClick={() => setArr(["4", ...arr, "5"])}>
         Кнопка3
-      </Button>
+      </Button> */}
     </>
   );
 };
