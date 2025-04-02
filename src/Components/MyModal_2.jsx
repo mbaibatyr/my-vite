3319f5e64c5faaa435ecf91854381e649ad73a90
mbaibatyr <murat_b@mail.ru>
2025-04-02 21:32:28 +0500

    +

--- src/Components/MyModal_2.jsx
+++ src/Components/MyModal_2.jsx
@@ -1,20 +1,20 @@
 import { Button, Modal, Popconfirm, message, notification } from "antd";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { increment, decrement, incrementByAmount } from "../Redux/counterSlice";
+import { increment, decrement, incrementByAmount } from "../Redux/counterSlice";
 
 const MyModal_2 = () => {
   const count = useSelector((state) => state.counter.value);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   return (
     <>
-      <h1>Count: {count}</h1>
-      {/* <button onClick={() => dispatch(increment())}>Increment</button>
+      <h3>Count: {count}</h3>
+      <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(incrementByAmount(5))}>
         Increment by 5
-      </button> */}
+      </button>
     </>
   );
 };
